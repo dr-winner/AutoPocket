41876968b8c0de6d91df00eb8b05fb2011ca23ba
dr-winner <dr@winner.com>
2026-03-13 18:41:09 +0000

    chore: update hardhat config for deployment
    Note: alfajores testnet may have DNS issues on some networks
    Use local machine to deploy if needed

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,7 +6,7 @@ module.exports = {
   solidity: "0.8.20",
   networks: {
     alfajores: {
-      url: "https://alfajores.gateway.tatum.io",
+      url: "https://alfajores-forno.celo-testnet.org",
       chainId: 44787,
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
     },
